58175679e79e68225936de58bebfd697a1c7707d
chaya <324863976@PC5-8>
2022-01-17 13:08:51 +0200

    middleWare

---
+++ MyFirstWebApiSite/RatingMiddleWare.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApiSite
+{
+ 
+    public class RatingMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        public RatingMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+
+
+
+
+    }
+}
